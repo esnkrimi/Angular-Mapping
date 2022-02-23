3fe33dd1c861284dac494ab32c9e59a43859122d
ehsan karimi <47683843+esnkrimi@users.noreply.github.com>
2022-02-23 12:29:22 +0330

    Update net-test.component.ts

--- mapping/net-test.component.ts
+++ mapping/net-test.component.ts
@@ -1,48 +1,91 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { fromEvent, of } from 'rxjs';
-import { tap, mergeMap, concatMap, exhaustMap, delay } from 'rxjs/operators';
-@Component({
-  selector: 'app-net-test',
-  templateUrl: './net-test.component.html',
-  styleUrls: ['./net-test.component.sass']
-})
-export class NetTestComponent implements OnInit, AfterViewInit {
-  @ViewChild('makeLike') makeLike: ElementRef;
-  constructor(private http: HttpClient) { }
-  ngOnInit(): void { }
-  ngAfterViewInit(): void {
-    fromEvent(this.makeLike.nativeElement, 'click').pipe(
-      tap(() => console.log('clicked')),
-      exhaustMap(() => this.httpGet()),
-      tap((i) => console.log(i))
-    ).subscribe((d) => console.log(d))
-  }
-  httpGet() {
-    return this.http.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN").pipe(
-      delay(5000)
-    );
-  }
-  concatMap() {
-    of(1, 2, 3, 4, 5).pipe(
-      concatMap((val) => this.postMan(val)),
-      tap((i) => console.log(i))
-    ).subscribe();
-  }
-  mergeMap() {
-    of(1, 2, 3, 4, 5).pipe(
-      mergeMap((val) => this.postMan(val)),
-      tap((i) => console.log(i))
-    ).subscribe();
-  }
+import { Component, Inject, OnInit } from '@angular/core'
+/*import { DEVICE_TYPE_IS_PC } from './app.module'
+import { globals } from '@bc/global-variables'
+import { LocalStorageService } from '@services/local-storage.service'
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core'*/
+import {
+  concatMap,
+  debounceTime,
+  distinctUntilChanged,
+  exhaustMap,
+  filter,
+  first,
+  from,
+  map,
+  mergeMap,
+  Observable,
+  of,
+  pipe,
+  skip,
+  switchMap,
+  take,
+  tap,
+} from 'rxjs'
+import { ApiService } from '../app/lib/apps/src/lib/service/api.service'
+import { HttpClient } from '@angular/common/http'
 
-  postMan(postId) {
-    let headers = new HttpHeaders({ 'Content-Type': 'application/json;charset=UTF-8' });
-    let option = { headers: headers };
-    let body = JSON.stringify({
-      title: 'test',
-      body: 'mybody'
-    });
-    return this.http.patch(`http://jsonplaceholder.typicode.com/posts/${postId}`, body, option);
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass'],
+})
+export class AppComponent implements OnInit {
+  c(d) {
+    console.log(d)
+  }
+  constructor(private http: HttpClient, private ApiService: ApiService) {}
+  ngOnInit(): void {
+    let obsC$ = of(1, 2, 3, 4).pipe(
+      tap((id) => this.c('concatMap')),
+      concatMap((id) =>
+        this.http.get(
+          'http://burjcrown.com/drm/web/index.php?time=19&id=8&do_id=' + id,
+        ),
+      ),
+    )
+
+    let obsM$ = of(1, 2, 3, 4).pipe(
+      tap((id) => this.c('mergeMap')),
+      mergeMap((id) =>
+        this.http.get(
+          'http://burjcrown.com/drm/web/index.php?time=19&id=8&do_id=' + id,
+        ),
+      ),
+    )
+
+    let obsS$ = of(1, 2, 3, 4).pipe(
+      tap((id) => this.c('switchMap')),
+      switchMap((id) =>
+        this.http.get(
+          'http://burjcrown.com/drm/web/index.php?time=19&id=8&do_id=' + id,
+        ),
+      ),
+    )
+
+    let obsE$ = of(1, 2, 3, 4).pipe(
+      tap((id) => this.c('exhaustMap')),
+      exhaustMap((id) =>
+        this.http.get(
+          'http://burjcrown.com/drm/web/index.php?time=19&id=8&do_id=' + id,
+        ),
+      ),
+    )
+
+    obsC$.subscribe((val) => {
+      console.log('concat DOID =', val[0]['do_id'])
+    })
+
+    obsM$.subscribe((val) => {
+      console.log('merge DOID =', val[0]['do_id'])
+    })
+
+    obsS$.subscribe((val) => {
+      console.log('switch DOID =', val[0]['do_id'])
+    })
+
+    obsE$.subscribe((val) => {
+      console.log('exhaustMap DOID =', val[0]['do_id'])
+    })
   }
 }
